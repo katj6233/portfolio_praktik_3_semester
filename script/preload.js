e5d5da0b0da9aa27a8c86bbd75f98d8d0b961635
Katja Levring <katj6233@stud.kea.dk>
2022-04-06 23:16:35 +0200

    preloader

--- script/preload.js
+++ script/preload.js
@@ -1,12 +1,20 @@
+// window.addEventListener("load", animation);
+
+// function animation() {
+//   const skelet = document.querySelector("body");
+
+//   if (!sessionStorage.getItem("isLoaded")) {
+//     skelet.classList.add("loaded");
+//     sessionStorage.setItem("isLoaded", true);
+//   } else {
+//     skelet.classList.add("no-load");
+//   }
+// }
+
+const body = document.querySelector("body");
+
 window.addEventListener("load", animation);
 
 function animation() {
-  const skelet = document.querySelector("body");
-
-  if (!sessionStorage.getItem("isLoaded")) {
-    skelet.classList.add("loaded");
-    sessionStorage.setItem("isLoaded", true);
-  } else {
-    skelet.classList.add("no-load");
-  }
+  body.classList.add("loaded");
 }
